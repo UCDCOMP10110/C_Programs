65a564d41c4f98f786e586258ab6f46b8d04b3c0
Opai Elsheikh <opai.elsheikh@ucdconnect.ie>
2020-10-02 15:34:55 +0100

    quize work

---
+++ Lab1Quiz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+
+  /* this uses comments this for printing hello world!! goodbye world!!*/
+  printf("Hello\t\t   world!!\n");
+  printf("goodbye\t\t world!!\n");
+
+  printf("one\n");
+  printf("two\n");
+  printf("one\n");
+  printf("two\n");
+  printf("three\n");
+  printf("one\ntwo");
+  printf("\n");
+  printf("\n");
+
+  printf("========================\n");
+  printf("=                      =\n");
+  printf("= She sells sea shells =\n");
+  printf("=                      =\n");
+  printf("========================\n");
+
+
+  printf("%d", 70);
+
+  return 0;
+}
