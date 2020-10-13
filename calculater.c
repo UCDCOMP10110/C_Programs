4b4a7d58b0b3742fbd05148ba676aa21a1224349
Opai Elsheikh <opai.elsheikh@ucdconnect.ie>
2020-10-13 11:48:23 +0100

    calculater

--- calculater.c
+++ calculater.c
@@ -1,24 +1,23 @@
-/*create a basic calculater*/
-#include <stdio.h>
-#include <math.h>
+/*write a program to read the base and hight of a triangle and display the area.
+the formula for the area of a triangle is [1/2(base*height)].*/
+ #include <stdio.h>
+ #include <math.h>
 
-int main(void)
-{
-    /* declare some variables */
-     int c;
-     double f;
-     double ans;
+ int main(void)
+ {
+     int base;
+     int height;
+     int ans;
 
-     printf("Please enter a floating point number: ");
-     scanf("%lf", &f);
-     printf("Please enter a integer point number: ");
-     scanf("%d", &c);
-     /* do my computation*/
-     ans = c*f;
+     printf("Please enter the length of the base: ");
+     scanf("lf", &base);
+     printf("Please enter the height:");
+     scanf("lf", &height);
 
-     /*write my answer to the console*/
+     ans = [1/2(base*height)];
 
-     printf("%d multiplied by %f = %f\n", c, f, ans);
 
-    return 0;
-}
+     return 0;
+
+
+ }
