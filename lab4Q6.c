c2222450b8ded3a2d7df19b72b876467cfd01adf
Opai Elsheikh <opai.elsheikh@ucdconnect.ie>
2020-10-19 20:42:30 +0100

    lab 4 Q6

---
+++ lab4Q6.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int main(void){
+
+    double P, R, Y;
+
+    double A;
+
+
+    printf("Please enter the amount:\n");
+    scanf("%lf", &A);
+    if( A < 0 ){
+        printf("Invalid amount.");
+        return 0;
+    }
+    printf("Please enter the principal:\n");
+    scanf("%lf", &P);
+    if( P < 0 ){
+        printf("Invalid principal.");
+        return 0;}
+     printf("Please enter the rate:\n");
+    scanf("%lf", &R);
+    if(!( 0 <= R && R <= 100)){
+        printf("Invalid rate.");
+        return 0;}
+
+     else{
+
+        Y = (log(pow((A/P),(1/12.0))))/(log(1+(R/(100*12.0))));
+        printf("The number of years is %.2lf\n", Y);
+
+    }
+
+
+
+
+    return 0;
+}
