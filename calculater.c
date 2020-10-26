ad55c7f8f9745a1f440c7050b9c74a2bc59bdfc5
opaielsheikh <72025112+opaielsheikh@users.noreply.github.com>
2020-10-26 16:08:17 +0000

    Update calculater.c

--- calculater.c
+++ calculater.c
@@ -3,20 +3,25 @@ the formula for the area of a triangle is [1/2(base*height)].*/
  #include <stdio.h>
  #include <math.h>
 
+ /*main function*/
  int main(void)
  {
+    /*assigning variables*/
      int base;
      int height;
      int ans;
-
+  
+    /*getting input from the user*/
      printf("Please enter the length of the base: ");
      scanf("lf", &base);
      printf("Please enter the height:");
      scanf("lf", &height);
-
+    /*computing it*/
      ans = [1/2(base*height)];
 
-
+    /*printing out the results*/
+     printf("the area is %d", ans);
+   /*stops the program*/
      return 0;
 
 
