0d4f1fd9618a985a9df9c000eea694066578a0e8
opaielsheikh <72025112+opaielsheikh@users.noreply.github.com>
2020-10-26 16:03:07 +0000

    Update Lab1Quiz.c

--- Lab1Quiz.c
+++ Lab1Quiz.c
@@ -8,6 +8,7 @@ int main(void)
   printf("Hello\t\t   world!!\n");
   printf("goodbye\t\t world!!\n");
 
+  /*usibg \n to make new lines*/
   printf("one\n");
   printf("two\n");
   printf("one\n");
@@ -16,14 +17,15 @@ int main(void)
   printf("one\ntwo");
   printf("\n");
   printf("\n");
-
+ 
+  /*making shapes using printf("") function*/
   printf("========================\n");
   printf("=                      =\n");
   printf("= She sells sea shells =\n");
   printf("=                      =\n");
   printf("========================\n");
 
-
+  /*this prints 70 in %d which %d is for integers*/
   printf("%d", 70);
 
   return 0;
