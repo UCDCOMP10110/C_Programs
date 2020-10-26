895c8f63e0535ed4936039341b55001253a6cad3
opaielsheikh <72025112+opaielsheikh@users.noreply.github.com>
2020-10-26 16:10:02 +0000

    Update first.c

--- first.c
+++ first.c
@@ -1,10 +1,11 @@
+/*including the file*/
 #include <stdio.h>
 
-
+/*the main function*/
 int main(void)
 {
-
+  /*printing the result to the user using printf() function*/
   printf("Hello world!!\n");
-
+  /*stops the programme*/
   return 0;
 }
