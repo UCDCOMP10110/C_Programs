b16696e63a5daa1c4a312ce91555bb4413584196
opaielsheikh <72025112+opaielsheikh@users.noreply.github.com>
2020-11-08 13:32:03 +0000

    Update lab4Q4.c

--- lab4Q4.c
+++ lab4Q4.c
@@ -1,40 +1,49 @@
-#include <stdio.h>
+#include <stdio.h>// program to find the principal 
 #include <math.h>
 
-
-int main(void){
-
-    double P, R, Y;
-    double K = 12;
-    double A;
-
-
-    printf("Please enter the amount:\n");
-    scanf("%lf", &A);
-    if( A < 0 ){
-        printf("Invalid amount.");
-        return 0;
-    }
-    printf("Please enter the rate:\n");
-    scanf("%lf", &R);
-    if( !(0 <= R && R <= 100) ){
-        printf("Invalid rate.");
-        return 0;}
-     printf("Please enter the number of years:\n");
-    scanf("%lf", &Y);
-    if(!(Y > 0)){
-        printf("Invalid number of years.");
-        return 0;}
-
-     else{
-
-        P = A/pow((1.0 + R/(100*K)) , Y*K);
-        printf("The principal is %.2f\n", P);
-
-    }
+int main(void)
+{
+         // declaring variables
+         double P,A,n,r;
+         double K = 12;
 
 
+         // input from user
+         printf("Please enter the amount:\n");
+         scanf("%lf", &A);
+         if(A < 0)
+         {
+             printf("invalid amount");
+             return 0;
+         }
+
+         printf("Please enter the rate:\n");
+         scanf("%lf", &r);
+         if(!(0<r && r<=100))
+         {
+             printf("invalid rate");
+             return 0;
+         }
+
+         printf("Please enter the number of years:\n");
+         scanf("%lf", &n);
+         if(!(n>0))
+         {
+             printf("invalid number of years:\n");
+             return 0;
+         }
 
 
-    return 0;
+         // compute
+
+         P = A/pow((1.0 + (r/(100*K))), n*K);
+
+
+         // result
+         printf("The principal is %.2f\n", P);
+
+
+
+
+         return 0;
 }
