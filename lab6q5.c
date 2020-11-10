6a92a779208b5f4a1f15217ed678d595b2b474e4
opaielsheikh <72025112+opaielsheikh@users.noreply.github.com>
2020-11-10 12:18:32 +0000

    lab 6 q5 sing Kadanes algorithm technique

--- lab6q5.c
+++ lab6q5.c
@@ -5,7 +5,7 @@
 
 int main(void)
 {
-// 
+// making the variables type
   int a[MAX_SIZE]; 
   int N, i;
   int m_e_h = 0;
@@ -18,7 +18,7 @@ int main(void)
   for (i=0;i<N;i++)
     scanf("%d", &a[i]);
 
-  /* complete your program here ... */
+  /*using Kadanes algorithm which saves the saves the max endin here with the curent every loop then uses max so far to see if its greater if so it save it their and using end and i to tell its location*/
 
   if (N == 0) {
     printf("Largest sum is 0 obtained from the subsequence [ ]");
@@ -37,6 +37,8 @@ int main(void)
       }
     }
 
+  
+    /* printing out the results */
     if (m_s_f < 0) {
       printf("Largest sum is %d obtained from the subsequence [ %d ]", m_s_f, m_s_f);
       return 0;
