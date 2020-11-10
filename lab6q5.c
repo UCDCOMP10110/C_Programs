ea0844dd7c18f743aab021cde59c26edc22da00e
opaielsheikh <72025112+opaielsheikh@users.noreply.github.com>
2020-11-10 12:13:04 +0000

    Create lab6q5.c

---
+++ lab6q5.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <limits.h>
+
+#define MAX_SIZE 100
+
+int main(void)
+{
+// 
+  int a[MAX_SIZE]; 
+  int N, i;
+  int m_e_h = 0;
+  int m_s_f = INT_MIN;
+  int start = 0;
+  int end = 0;
+
+  /* read in the array */
+  scanf("%d", &N);
+  for (i=0;i<N;i++)
+    scanf("%d", &a[i]);
+
+  /* complete your program here ... */
+
+  if (N == 0) {
+    printf("Largest sum is 0 obtained from the subsequence [ ]");
+    return 0;
+  }
+
+    for (i = 0; i < N; i++) {
+      m_e_h = m_e_h + a[i];
+      if (m_e_h < a[i]){
+        m_e_h = a[i];
+        start = i;
+      }
+      if (m_s_f < m_e_h) {
+        m_s_f = m_e_h;
+        end = i;
+      }
+    }
+
+    if (m_s_f < 0) {
+      printf("Largest sum is %d obtained from the subsequence [ %d ]", m_s_f, m_s_f);
+      return 0;
+    }
+    end = end + 1;
+
+    printf("Largest sum is %d obtained from the subsequence ", m_s_f);
+    printf("[ ");
+
+    for (i = start; i < end; i++) {
+      printf("%d ", a[i]);
+    }
+
+    printf("]");
+
+  return 0;
+}
