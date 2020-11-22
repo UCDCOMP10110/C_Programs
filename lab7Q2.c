448f582aeeb02961ae3e3b9c1876f4126c98bdac
opaielsheikh <72025112+opaielsheikh@users.noreply.github.com>
2020-11-22 12:37:44 +0000

    Update lab7Q2.c

--- lab7Q2.c
+++ lab7Q2.c
@@ -6,7 +6,7 @@
 
 int main(void)
 
-{ 
+{
 
   int a[MAX_SIZE];
   int N;
@@ -23,14 +23,19 @@ int main(void)
     } while (N<3 || N>MAX_SIZE+2);
 
   /* write your solution here ... */
-    int i, j, isPrime;
+    int i, j,p, isPrime;
     L = 2;
     int temp = 2;
     a[0] = 2;
     a[1] = 3;
-    for(i=5; i<=N; i = i + 2)
-    {
 
+    for(i=0; i<N-2; i++)
+		a[i] = i+2;
+
+    //looping through the numbers to check weather it is a prime or not 
+    for(i=5; i < N; i++)
+    {
+        //using a method similar to boolean (flag)
         isPrime = 1;
 
 
@@ -47,7 +52,7 @@ int main(void)
             }
         }
 
-        /* If the number is prime then print */
+        /* If the number is prime it save its place in the array */
         if(isPrime==1)
         {
             a[temp] = i;
