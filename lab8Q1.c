5ec496d9f97f152b3e4979d4fee6e27e7d3627f0
opaielsheikh <72025112+opaielsheikh@users.noreply.github.com>
2020-11-29 15:52:31 +0000

    program to find the value in an integer array a[] of length N that occurs at least (N/2 + 1) times

--- lab8Q1.c
+++ lab8Q1.c
@@ -1,76 +1,42 @@
 #include <stdio.h>
-#include <stdbool.h>
+#define MAX_SIZE 5000
 
-#define MAX_SIZE 500
+
 
 int main(void)
 {
-  // declaring variables
-  int a[MAX_SIZE];
-  int unique_vals[MAX_SIZE], counts[MAX_SIZE];
-  int i, j;
-  bool counted[MAX_SIZE];
-  int num_unique = 0;
-  int v,N, max = 0, ind;
+  int N; //size of teh array
+  int a[MAX_SIZE]; //the array
+  int value; // contains the value
+  int count; //keeps count of each element 
+  int i; //keeps count of the array
   
-  // getting input from the user for the size
-  scanf("%d", &N);
-  
-  // getting input from the user for the numbers in the array
+  scanf("%d",&N);
   for (i=0;i<N;i++)
     {
-        counted[i] = false;
-        counts[i] = 0;
-        scanf("%d", &a[i]);
+      scanf("%d", &a[i]);
     }
-   
-   //outer loop, loops through the whole numbers in the arrays
-   for(i=0;i<N;i++)
-   {
-      // setting each value in V
-      v = a[i];
-      //checking the number if it has been counted or not
-      if(!counted[i])
-      {
-          //adding the amount it counted
-          counts[num_unique]++;
-          counted[i] = true;
-          //looping throught the whole array again to check if it is repeated if so we incremanate it
-          for(j=i+1;j<N;j++)
-          {
-              if(a[j] == v)
-              {
-                  counted[j] = true;
-                  counts[num_unique]++;
-              }
-          }
-          //saving the number v in an unique array that holds all the unique values 
-        unique_vals[num_unique++] = v;
-      }
-   }
-   //looping again to save the most repeated number in a variable called max and saving that number in ind 
-   for(i=0;i<num_unique;i++)
-   {
-       if(counts[i] > max)
-       {
-          ind = unique_vals[i]; 
-          max = counts[i];
-       }
-   }
-   //conditions 
-   if(max > N/2)
-     {
-       printf("%d appears more than %d times in the array.",ind, N/2);
-      }
-      else if(max <= N/2)
-           {
-              printf("No value appears more than %d times in the array.", N/2);
-           }
+
+  count = 0; // set count to 0
+  for (i=0;i<N;i++) //loops through the array
+    {
+      if (count == 0) //checks if count is eqaul to zero if so count it
+	value = a[i]; //saves the value number in vale
+      if (a[i]==value) //checks if it occured again incremeant it
+	count++;
+      else // if not we desincreament 
+	count--;
+    }
   
-    
-    
+  count = 0; //doing the count around it
+  for (i=0;i<N;i++)
+    if (a[i] == value)
+      count++;
 
-    
+  if (count>=(N/2+1)) //print out if the value is greater then half of the arrary
+    printf("%d appears more than %d times in the array.\n", value, N/2);
+  else //if not prints this out
+    printf("No value appears more than %d times in the array.\n", N/2);
 
-    return 0;
+  return 0;
 }
